7821bcdb0e3ca7959bfecb49ebb4af42fe930c53
Deele <deele@tuta.io>
2018-08-10 21:34:03 +0300

    Create foundation for site footer widget and place it in layout footer

---
+++ widgets/site_footer/views/siteFooter.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Site footer widget body view
+ */
+
+/**
+ * @var \yii\web\View $this
+ */
+
+?>Sadarbībā ar <a href="http://www.lab.lv/" target="_blank">Latvijas Astronomijas Biedrību</a> | 2018
