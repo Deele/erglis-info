273c11bbacc6c2421e121c02a234a01dad0d4682
Lina <lina.sarma@gmail.com>
2018-08-12 03:05:35 +0300

    building map

--- widgets/events_map/assets/dist/eventsMap.js
+++ widgets/events_map/assets/dist/eventsMap.js
@@ -1,60 +1,65 @@
 window.yii.eventsMapWidget = ($ => {
-    "use strict";
+  "use strict";
 
-    /**
-     * Widget type settings
-     */
-    let pub = {
-        settings: {
-            name: ''
-        }
-    };
-
-    /**
-     * Registers new instance of widget
-     * @param {string} widgetId
-     * @param {object} [options]
-     */
-    pub.register = (widgetId, options) => {
-        return new EventsMapWidget(widgetId, pub, options);
-    };
-
-    /**
-     * Company parameter type category
-     *
-     * @property {int} id
-     * @property {string} title
-     * @property-read {jQuery} element
-     * @property {boolean} isSelected
-     */
-    class EventsMapWidget extends YiiWebAppWidget {
-
-        /**
-         * @param {string} id
-         * @param {object} parent
-         * @param {object} [options]
-         */
-        constructor(id, parent, options) {
-            super(id, parent, options);
-
-            this.init(options);
-        }
-        afterInit () {
-            "use strict";
-            const w = this;
-            if (w.initWidgetElement()) {
-                w.run();
-            }
-        }
-        run () {
-            const w = this;
-            w.renderState();
-        }
-        renderState () {
-            const w = this;
-        }
+  /**
+  * Widget type settings
+  */
+  let pub = {
+    settings: {
+      name: ''
     }
+  };
 
-    return pub;
+  /**
+  * Registers new instance of widget
+  * @param {string} widgetId
+  * @param {object} [options]
+  */
+  pub.register = (widgetId, options) => {
+    return new EventsMapWidget(widgetId, pub, options);
+  };
+
+  /**
+  * Company parameter type category
+  *
+  * @property {int} id
+  * @property {string} title
+  * @property-read {jQuery} element
+  * @property {boolean} isSelected
+  */
+  class EventsMapWidget extends YiiWebAppWidget {
+
+    /**
+    * @param {string} id
+    * @param {object} parent
+    * @param {object} [options]
+    */
+
+
+    constructor(id, parent, options) {
+      super(id, parent, options);
+      this.init(options);
+
+
+    }
+    afterInit () {
+      "use strict";
+      const w = this;
+      if (w.initWidgetElement()) {
+        w.run();
+      }
+
+
+    }
+    run () {
+      const w = this;
+      w.renderState();
+
+
+    }
+    renderState () {
+      const w = this;
+    }
+  }
+  return pub;
 })(window.jQuery);
-
